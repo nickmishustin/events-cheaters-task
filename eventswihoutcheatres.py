3120dc7f8404f405d2a5241b9032dd33e1626f69
nick <nickmishustin1@gmail.com>
2021-06-23 16:47:49 +0300

    code formatting to PEP8

--- eventswihoutcheatres.py
+++ eventswihoutcheatres.py
@@ -1,20 +1,28 @@
+from datetime import date, datetime
 import pandas as pd
 import sqlite3
-from datetime import date, datetime
+
 
 class EventsWithoutCheaters:
-    df = pd.DataFrame()   
 
-    def __init__(self, cheaters_path, result_path, server_path, client_path, target_date):
+    df = pd.DataFrame()
+
+    def __init__(
+        self,
+        cheaters_path,
+        result_path,
+        server_path,
+        client_path,
+        target_date
+    ):
         self.cheaters_path = cheaters_path
         self.result_path = result_path
         self.target_date = target_date
         self.server_path = server_path
         self.client_path = client_path
 
-
     def __get_target_df(self, path):
-        df= pd.read_csv(path, delimiter=',', header=0)
+        df = pd.read_csv(path, delimiter=',', header=0)
         df['date'] = df['timestamp'].apply(
             lambda T: datetime.fromtimestamp(T).date()
         )
@@ -22,11 +30,16 @@ class EventsWithoutCheaters:
         df.drop('date', axis=1, inplace=True)
         return df
 
-
     def __remove_cheaters(self):
         sql_connection = sqlite3.connect(self.cheaters_path)
-        df_cheaters =  pd.read_sql_query('SELECT * FROM cheaters', sql_connection)
-        self.df = df_cheaters.merge(self.df, left_on='player_id', right_on='player_id')
+        df_cheaters = pd.read_sql_query(
+            'SELECT * FROM cheaters',
+            sql_connection
+        )
+        self.df = df_cheaters.merge(
+            self.df, left_on='player_id',
+            right_on='player_id'
+        )
         sql_connection.close()
         self.df['ban_time'] = self.df['ban_time'].apply(
             lambda T: datetime.strptime(T, '%Y-%m-%d %H:%M:%S')
@@ -34,20 +47,24 @@ class EventsWithoutCheaters:
         self.df['server_time'] = self.df['timestamp'].apply(
             lambda T: datetime.fromtimestamp(T)
         )
-        self.df['datetime_diff']  = (self.df['ban_time'] - self.df['server_time']).dt.days 
+        self.df['datetime_diff'] = (
+            self.df['ban_time'] - self.df['server_time']
+        ).dt.days
         self.df = self.df[self.df['datetime_diff'] >= 1]
-        
         self.df.drop(
             ['ban_time', 'server_time', 'datetime_diff'],
             axis=1,
             inplace=True
         )
 
-
     def set_df(self):
         df_server = self.__get_target_df(self.server_path)
         df_client = self.__get_target_df(self.client_path)
-        self.df = df_server.merge(df_client, left_on='error_id', right_on='error_id')
+        self.df = df_server.merge(
+            df_client,
+            left_on='error_id',
+            right_on='error_id'
+        )
         self.df.rename(
             columns={
                 'timestamp_x': 'timestamp',
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from sqlite3.dbapi2 import Timestamp
-import pandas as pd
 from datetime import date
-from eventswihoutcheatres import EventsWithoutCheaters
 import tracemalloc
-import sys
+from eventswihoutcheatres import EventsWithoutCheaters
 
 
 def display_memory_usage(snapshot, key_type='lineno'):
@@ -15,19 +12,18 @@ def display_memory_usage(snapshot, key_type='lineno'):
     total = sum(stat.size for stat in top_stats)
     print("Total allocated size: %.1f KiB" % (total / 1024))
 
-
 if __name__ == '__main__':
     tracemalloc.start()
 
     e = EventsWithoutCheaters(
         cheaters_path='cheaters.db',
         result_path='result.db',
-        server_path='server.csv', 
+        server_path='server.csv',
         client_path='client.csv',
         target_date=date(year=2021, month=3, day=7)
     )
-
     e.set_df()
     e.write_to_db()
+
     snapshot = tracemalloc.take_snapshot()
     display_memory_usage(snapshot)
